70d787df79e466315baf19c0d8c6fa975ec83c0f
gumarm <guillaume.martin555@gmail.com>
2017-01-14 22:30:04 -0500

    Start setup of http call.

--- src/elm/Components/Tweets/TwitterContainer.elm
+++ src/elm/Components/Tweets/TwitterContainer.elm
@@ -2,15 +2,50 @@ module Components.Tweets.TwitterContainer exposing (..)
 
 import Maybe
 import Html exposing (..)
+import Http
+import Json.Decode as Decode
+
 import Components.Tweets.RenderTweets exposing (renderTweets)
 
-model : Maybe (List String)
-model = Nothing
+type Msg = ReceiveTweets (Result Http.Error String)
 
-loadTweets : String -> Html a
-loadTweets user = 
-  div [] [text(user), div [] [
-    model
+type alias Model = {
+  user: String,
+  tweets: Maybe (List String)
+}
+
+model : Model
+model = Model "trump" Nothing
+
+update : Msg -> Model -> Model
+update msg model = 
+  case msg of
+    ReceiveTweets (Ok data) ->
+      Model model.user (Just (extractTweets data))
+    ReceiveTweets (Err _) ->
+      Model model.user Nothing
+
+
+view : Model -> Html a
+view model = 
+  div [] [text(model.user), div [] [
+    model.tweets
       |> Maybe.map renderTweets
       |> Maybe.withDefault (div [] [text("No tweets!")])
     ]]
+
+loadTweets : String -> Cmd Msg
+loadTweets user =
+  let
+    url =
+      "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ user
+  in
+    Http.send ReceiveTweets (Http.get url decodeGifUrl)
+
+decodeGifUrl : Decode.Decoder String
+decodeGifUrl =
+  Decode.at ["data", "image_url"] Decode.string
+
+extractTweets : String -> List String
+extractTweets data = 
+  [data]
