1d1d2e387acfdfd8ae913e59457310baff33bdbb
gumarm <guillaume.martin555@gmail.com>
2017-01-16 20:24:04 -0500

    Style the app. Add the markdown package to print html without it being escaped by Elm

--- src/elm/Components/Tweets/RenderTweets.elm
+++ src/elm/Components/Tweets/RenderTweets.elm
@@ -3,13 +3,14 @@ module Components.Tweets.RenderTweets exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import List
+import Markdown
 
 renderTweet : String -> Html a
 renderTweet tweet =
-  div [class "tweet"][text (tweet)]
+  div [class "ui raised segment"][Markdown.toHtml [] tweet]
 
 renderTweets : List String -> Html a
 renderTweets tweets =
-  div [class "tweets"]
+  div []
   (tweets
   |> List.map renderTweet)
